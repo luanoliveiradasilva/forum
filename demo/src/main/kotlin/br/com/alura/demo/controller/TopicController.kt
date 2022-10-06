f4b10b49cd017e9d25a9502b3f6f1c8cb5224592
Luan Nascimento <luanoliveira1107@gmail.com>
2022-10-05 21:18:34 -0300

    feat-[#4] - Camada service.
    Task: [#1] - Implementação da camada service com regra de negócio.

--- demo/src/main/kotlin/br/com/alura/demo/controller/TopicController.kt
+++ demo/src/main/kotlin/br/com/alura/demo/controller/TopicController.kt
@@ -1,4 +1,4 @@
-package br.com.alura.demo.domain.controller
+package br.com.alura.demo.controller
 
 import br.com.alura.demo.domain.model.Topic
 import br.com.alura.demo.domain.service.TopicService
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import javax.websocket.server.PathParam
 
 @RestController
 @RequestMapping("/v1/topics")
